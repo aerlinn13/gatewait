b586f10c36982887561006e664581cd91141e62b
Danil Chernyshev <danil@chernyshev.pro>
2019-10-09 22:35:10 +0100

    initial commit

---
+++ gatewait/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gatewait
+//
+//  Created by Danil on 09.10.2019.
+//  Copyright © 2019 Danil Chernyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
